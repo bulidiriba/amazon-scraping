c471635045444b75e1e73d5ce2c1b8595d6dee9e
bulidiriba <bulidiriba@gmail.com>
2022-11-15 15:22:30 +0300

    implement updating the google sheet data with deleting the old one first

--- scrape.js
+++ scrape.js
@@ -33,38 +33,32 @@ console.log("Reading data....");
             spreadsheetId,
             range: "output!A:B"
         });
-        let data = readData.data.values;
+        const data = readData.data.values;
+        const data_len = data.length;
         data.forEach((item) => {
             sites.push(item)
         });
 
-    } catch(error) {
-        console.log(`Error : ${error.message}`)
-    }
+        var options = {
+            headless: false,
+            args: [
+                "--disable-extensions-except=~/.config/google-chrome/Default/Extensions/dcmindjgpiimpmkgmabhkflfaiimioea/2.39_0",
+                "--load-extension=~/.config/google-chrome/Default/Extensions/dcmindjgpiimpmkgmabhkflfaiimioea/2.39_0",
+            ]
+        }
 
-    var options = {
-        headless: false,
-        args: [
-            "--disable-extensions-except=~/.config/google-chrome/Default/Extensions/dcmindjgpiimpmkgmabhkflfaiimioea/2.39_0",
-            "--load-extension=~/.config/google-chrome/Default/Extensions/dcmindjgpiimpmkgmabhkflfaiimioea/2.39_0",
-        ]
-    }
+        const browser = await launch(options);
+        const page = await browser.newPage();
+        await page.waitForTimeout(3000);
 
-    const browser = await launch(options);
-    const page = await browser.newPage();
-    await page.waitForTimeout(3000);
-
-    
-
-    // Read from the spreadsheet
-    try {
+        // Read from the spreadsheet
         await page.goto("https://www.google.com/", {
             waitUntil: "load",
             timeout: 0
         });
 
         await page.screenshot({ path: "screenshots/0-google.png"});
-        let all_sites = [];
+        let updated_data = [];
         
         for (let i=1; i < 5; i++) {
             try {
@@ -81,13 +75,53 @@ console.log("Reading data....");
                     return "NO"
                 });
                 console.log(`found: ${found}`);
-                all_sites.push({"Amazon ASIN URL": sites[i][1], "IP Alert Found": found, "IP Alert LastCheck": new Date().toLocaleString()})
+                updated_data.push(sites[i][0], [sites[i][1], found, new Date().toLocaleString()])
             } catch(error) {
                 console.log(`Error Occured : ${error.message}`)
             }
         }
 
+        // Before updating first delete the existing data
+        const deleteRequest = {
+            auth: auth,
+            spreadsheetId: spreadsheetId,
+            resource: {
+                'requests': [
+                    {
+                        'deleteDimension': {
+                            'range': {
+                                'sheetId': 0,
+                                'dimension': 'ROWS',
+                                'startIndex': 1,
+                                'endIndex': data_len + 1
+                            }
+                        }
+                    }
+                ]
+            }
+        }
+
+        googleSheetsInstance.spreadsheets.batchUpdate(deleteRequest, function(err, response) {
+            if (err) {
+                console.log(err);
+            }
+            console.log("Deleted.");
+        });
+
+        // Write data into the google sheets
+        googleSheetsInstance.spreadsheets.values.append({
+            auth,
+            spreadsheetId,
+            range: "output!A:D",
+            valueInputOption: "USER_ENTERED",
+            resource: {
+                values: updated_data
+            },
+        });
+
     } catch(error) {
         console.log(`Error : ${error.message}`);
+    } finally {
+        await browser.close();
     }
 })();
