f78b5559aa879b4d1e93012abbab661b899aed41
bulidiriba <bulidiriba@gmail.com>
2022-11-18 16:40:42 +0300

    add to google sheet if error occurred too

--- scrape.js
+++ scrape.js
@@ -61,7 +61,7 @@ console.log("\n...........Automated Scripts to check IP Alert Found for List of 
 
         let updated_data = [];
         let error_data = [];
-        const start_index = 1001;
+        const start_index = 8701;
         const end_index = 10000;
 
         console.log(`\nChecking IP Alert Found for Amazon URLs from row ${start_index} to ${end_index}`);
@@ -82,42 +82,44 @@ console.log("\n...........Automated Scripts to check IP Alert Found for List of 
                 console.log(`IP Alert Last Check : ${lastCheck}`);
                 updated_data.push([sites[i][0], sites[i][1], found, lastCheck])
                 
-                if (i % 50 === 0) {
-                    //console.log("updated data: ", updated_data);
-                    await page.waitForTimeout(10000);
-                    console.log("\nWriting updated output data to google sheet........");
-                    // Write data into the google sheets
-                    try{
-                        googleSheetsInstance.spreadsheets.values.append({
-                            auth,
-                            spreadsheetId,
-                            range: "output!A:D",
-                            valueInputOption: "USER_ENTERED",
-                            resource: {
-                                values: updated_data
-                            },
-                        });
-
-                        googleSheetsInstance.spreadsheets.values.append({
-                            auth,
-                            spreadsheetId,
-                            range: "error!A:C",
-                            valueInputOption: "USER_ENTERED",
-                            resource: {
-                                values: error_data
-                            },
-                        });
-                        updated_data = [];
-                        error_data = [];
-                        console.log("\nGoogle sheet succesfully updated.");
-                    } catch(error) {
-                        console.log(`Error: ${error.message}`);
-                    }           
-                }
             } catch(error) {
                 console.log(`Error Occured : ${error.message}`)
                 error_data.push([i, sites[i][0], sites[i][1]]);
             }
+
+            if (i % 50 === 0) {
+                //console.log("updated data: ", updated_data);
+                await page.waitForTimeout(10000);
+                console.log("\nWriting updated output data to google sheet........");
+                // Write data into the google sheets
+                try{
+                    googleSheetsInstance.spreadsheets.values.append({
+                        auth,
+                        spreadsheetId,
+                        range: "output!A:D",
+                        valueInputOption: "USER_ENTERED",
+                        resource: {
+                            values: updated_data
+                        },
+                    });
+
+                    googleSheetsInstance.spreadsheets.values.append({
+                        auth,
+                        spreadsheetId,
+                        range: "error!A:C",
+                        valueInputOption: "USER_ENTERED",
+                        resource: {
+                            values: error_data
+                        },
+                    });
+                    updated_data = [];
+                    error_data = [];
+                    console.log("\nGoogle sheet succesfully updated.");
+                } catch(error) {
+                    console.log(`Error: ${error.message}`);
+                }           
+            }
+            await page.waitForTimeout(5000);
         }
 
         const end_time = new Date();
